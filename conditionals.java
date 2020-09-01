bbcf92c40b551880b10a409d3fa7be3dc8552e2d
Abhishek Chandra <51490584+abhishekchandra2522k@users.noreply.github.com>
2020-09-01 21:08:10 +0530

    Add files via upload

---
+++ conditionals.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class conditionals{
+public static void main(String args[]){
+	int x = 10;
+	
+	if(x % 2 == 0)
+	{
+		System.out.println(x + " is even.");
+	}
+	else
+	{
+		System.out.println(x + " is odd.");
+	}
+	
+	System.out.println("Hardwork is better than smartwork.");
+}
+}
