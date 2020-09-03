aa4240a7365a542ab285083b1dc25b9456100a7a
Abhishek Chandra <51490584+abhishekchandra2522k@users.noreply.github.com>
2020-09-03 17:52:46 +0530

    Add files via upload

---
+++ dowhileloop.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class dowhileloop{
+public static void main(String args[]){
+	int i = 1;
+	/*
+	while(i <= 9){
+	System.out.println(i);
+	i++;
+	}
+	
+	for(int i = 1; i <= 9; i++){
+	System.out.println(i);
+	}*/
+	
+	do{
+		System.out.println(i);
+		i++;
+	}while(i <= 9);
+	System.out.println("Done.");
+}
+
+}
