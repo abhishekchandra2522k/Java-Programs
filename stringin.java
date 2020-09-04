c49d4f08078f8db7964427f14dd7dcc6e8df1dcc
Abhishek Chandra <51490584+abhishekchandra2522k@users.noreply.github.com>
2020-09-04 14:16:22 +0530

    Add files via upload

---
+++ stringin.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class stringin{
+public static void main(String args[]){
+	Scanner scn = new Scanner(System.in);
+	String name = scn.nextLine();
+	System.out.println(name);
+}
+}
