107321871b171d5425912a45cd5f06fdcb13c4f4
Abhishek Chandra <51490584+abhishekchandra2522k@users.noreply.github.com>
2020-09-04 14:11:13 +0530

    Add files via upload

---
+++ input.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class input{
+public static void main(String args[]){
+	Scanner scr = new Scanner(System.in);
+	int n = scr.nextInt();
+for(int i = 0; i <= n; i++)
+	System.out.println(i);	
+}
+}
