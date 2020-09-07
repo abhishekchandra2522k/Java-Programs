32bace8fb857fd9967e8a5c0e660fc2f45ffe2fe
Abhishek Chandra <51490584+abhishekchandra2522k@users.noreply.github.com>
2020-09-07 22:02:35 +0530

    Add files via upload

---
+++ rotate.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class rotate{
+public static void main(String args[]){
+	Scanner scn = new Scanner(System.in);
+	
+	int n = scn.nextInt();
+	int k = scn.nextInt();
+	
+	int temp = n;
+	int count = 0;
+	while(temp > 0){
+	temp = temp / 10;
+	count++;
+	}
+	
+	k = k % count;
+	if(k < 0){
+		k = k + count;
+	}
+	
+	int div = 1;
+	int mult = 1;
+	for(int i = 1; i <= count; i++){
+		if(i <= k){
+			div = div * 10; 
+		}
+		else{
+			mult = mult * 10;
+		}
+	}
+	
+	int q = n / div;
+	int r = n % div;
+	
+	int rot = r * mult + q;
+	System.out.println(rot);
+}
+
+}
