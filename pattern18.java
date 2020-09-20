beac14c62fd22f743f65f140ff898954825a8b83
Abhishek Chandra <51490584+abhishekchandra2522k@users.noreply.github.com>
2020-09-20 13:24:43 +0530

    Add files via upload

---
+++ pattern18.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class pattern18{
+
+public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+
+     // write ur code here
+    int n = scn.nextInt();
+    int sp = 0;
+    int st = n;
+    
+    for(int i = 1;i<=n;i++){
+        for(int j = 1;j<=sp;j++){
+            System.out.print("\t");
+        }
+        for(int j = 1;j<=st;j++){
+            if(i > 1 && i <= n/2 && j > 1 && j < st){
+                System.out.print("\t");
+            }else{
+                System.out.print("*\t");
+            }
+        }
+        if(i <= n/2 ){
+            sp++;
+            st-=2;
+        }else{
+            st+=2;
+            sp--;
+        }
+        System.out.println();   
+        
+    }
+ }
+}
+
+
+/*
+
+input = 7;
+
+output : 
+
+*	*	*	*	*	*	*	
+	*				*	
+		*		*	
+			*	
+		*	*	*	
+	*	*	*	*	*	
+*	*	*	*	*	*	*	
+
+*/
