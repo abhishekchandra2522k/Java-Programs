a419a03a6acc17bbc03bd03d0477e145732a1a99
Abhishek Chandra <1ac23456789@gmail.com>
2020-09-26 12:34:23 +0530

    Add files via upload

---
+++ anybaseaddition.java
@@ -0,0 +1,53 @@
+import java.util.*;
+  
+  public class anybaseaddition{
+  
+  public static void main(String[] args) {
+      Scanner scn = new Scanner(System.in);
+      int b = scn.nextInt();
+      int n1 = scn.nextInt();
+      int n2 = scn.nextInt();
+  
+      int d = getSum(b, n1, n2);
+      System.out.println(d);
+   }
+  
+   public static int getSum(int b, int n1, int n2){
+    int dec1 = anybasetodecimal(n1,b);
+    int dec2 = anybasetodecimal(n2,b);
+    
+    int sum = dec1 + dec2;
+    int rsum = decimaltoanybase(sum,b);
+    return rsum;
+    
+   }
+       public static int anybasetodecimal(int n, int b){
+        int rv = 0;
+        int p = 1;
+        while(n > 0){
+            int dig = (n%10);
+            n /= 10;
+            rv += dig * p;
+            
+            
+            p = p * b;
+        }
+        
+        return rv;
+  }
+  
+    public static int decimaltoanybase(int n, int b){
+       int dn = 0;
+       for(int i = 1;n > 0;){
+       int dig = (n % b);
+       n = n / b;
+       dn += dig * i;
+       
+       i = i * 10;
+       
+       }
+       return dn;
+      }   
+          
+      
+  }
