aa79fab853f2c968d93583a6aaf199d94955de14
Abhishek Chandra <1ac23456789@gmail.com>
2020-11-07 22:18:59 +0530

    Add files via upload

---
+++ wave_traversal.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class wave_traversal{
+	public static void main(String args[]){
+		Scanner scn = new Scanner(System.in);
+
+		int r = scn.nextInt();
+		int c = scn.nextInt();
+
+		int[][] arr = new int[r][c];
+
+		for(int i = 0; i < arr.length; i++){
+			for(int j = 0; j < arr[0].length; j++){
+				arr[i][j] = scn.nextInt();
+			}
+		}
+
+
+		for(int j = 0; j < arr[0].length; j++){
+				if(j % 2 == 0){
+					for(int i = 0; i < arr.length; i++){
+						System.out.println(arr[i][j] + " ");
+					}
+				}else{
+					for(int i = arr.length - 1; i >= 0; i--){
+						System.out.println(arr[i][j] + " ");
+					}
+				}
+		}
+	}
+}
+
+/* 
+Input : 
+2
+2
+11 12
+21 22
+
+Output:
+
+11
+21
+22
+12
+*/
